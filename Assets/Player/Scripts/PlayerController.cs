c50191da757bbab407aaaa6ce4a7e65cd2ffba92
fall211 <94519579+fall211@users.noreply.github.com>
2023-06-11 17:57:32 -0700

    tileset collisions

--- Assets/Player/Scripts/PlayerController.cs
+++ Assets/Player/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@ using System;
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
 
 
 public class PlayerController : NetworkBehaviour
@@ -10,6 +11,9 @@ public class PlayerController : NetworkBehaviour
     private Vector2 _movementInput;
     private Vector3 _lastPosition;
     private Vector3 _velocity;
+    private ContactFilter2D _contactFilter2D = new ContactFilter2D();
+    private List<RaycastHit2D> _hitBuffer = new List<RaycastHit2D>();
+    [SerializeField] private Transform _raycastOrigin;
 
     public float moveSpeed = 1f;
 
@@ -28,7 +32,16 @@ public class PlayerController : NetworkBehaviour
         if (!IsOwner) return;
         _lastPosition = transform.position;
         
-        TryMove(_movementInput);
+        if (_movementInput != Vector2.zero){
+            bool success = TryMove(_movementInput);
+            if (!success){
+                success = TryMove(new Vector2(_movementInput.x, 0));
+                if (!success){
+                    success = TryMove(new Vector2(0, _movementInput.y));
+                }
+            }
+        }
+
 
         _velocity = (transform.position - _lastPosition) / Time.deltaTime;
         UpdateAnimation(_velocity);
@@ -36,11 +49,17 @@ public class PlayerController : NetworkBehaviour
 
     }
 
-    private void TryMove(Vector2 direction)
+    private bool TryMove(Vector2 direction)
     {
+        _contactFilter2D.SetLayerMask(LayerMask.GetMask("Obstacles"));
+        var hitCount = Physics2D.Raycast(_raycastOrigin.position, direction, _contactFilter2D, _hitBuffer, (moveSpeed * Time.deltaTime) + 0.1f);
+        if (hitCount > 0) return false;
+
         var moveHere = new Vector3(direction.x, direction.y, 0);
         moveHere *= Time.deltaTime * moveSpeed;
         transform.position += moveHere;
+
+        return true;
     }
 
 
